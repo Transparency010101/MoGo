8321486babe1fc2f6e9fe79af04be50dadd21ccf
Transparency010101 <ossys1010110@gmail.com>
2021-09-16 00:20:39 +0300

    add new file

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,17 @@ let path = {
         fonts: project_folder + "/fonts/",
     },
     src: {
-        html: [src_folder + "/*.{html, kit}", "!" + src_folder + "/_*.{html, kit}"],
+        html: [src_folder + "/*.html", "!" + src_folder + "/_*.html"],
         css: src_folder + "/sass/style.scss",
         js: src_folder + "/js/all.js",
-        img: src_folder + "/image/**/*.{jpg, png, svg, gif, ico, webp}",
+        img: src_folder + "/image/**/*.+(png|jpg|gif|ico|svg|webp)",
         fonts: src_folder + "/fonts/*.ttf",
     },
     watch: {
-        html: src_folder + "/**/*.{html, kit}",
+        html: src_folder + "/**/*.html",
         css: src_folder + "/sass/**/*.scss",
         js: src_folder + "/js/**/*.js",
-        img: src_folder + "/image/**/*.{jpg, png, svg, gif, ico, webp}",
+        img: src_folder + "/image/**/*.+(png|jpg|gif|ico|svg|webp)",
     },
     clean: "./" + project_folder + "/"
 }
@@ -35,7 +35,10 @@ let { src, dest } = require("gulp"),
     autoprfixer = require("gulp-autoprefixer"),
     group_media = require("gulp-group-css-media-queries"),
     clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename");
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify-es").default,
+    imagemin = require("gulp-imagemin"),
+    webp = require("gulp-imagemin");
 
 // $ Function для сихронизации с браузером
 function browserSync(params) {
@@ -79,7 +82,37 @@ function css() {
         .pipe(dest(path.build.css))
         .pipe(browsersync.stream())
 }
-
+// $ Function js
+function js() {
+    return src(path.src.js)
+        .pipe(fileinclude())
+        .pipe(dest(path.build.js))
+        .pipe(
+            uglify()
+        )
+        .pipe(
+            rename({
+                extname: ".min.js"
+            })
+        )
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream())
+}
+// $ Function Для картинок
+function images() {
+    return src(path.src.img)
+        .pipe(webp())
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true,
+            optimizationLevel: 3,
+        }))
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
 // $ Function удаление поворных файлов
 function clean(params) {
     return del(path.clean);
@@ -89,11 +122,15 @@ function clean(params) {
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.img], images);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
